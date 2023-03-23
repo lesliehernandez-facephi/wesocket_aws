03268c9a0233ff137d91ebc551875c6ffc0d3755
Ericka <erickaramos@facephi.com>
2023-03-23 10:04:08 +0100

    added resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,23 +17,23 @@
 
 provider aws {
   region = var.aws_region
-
+  # profile = var.profile
 }
 
 
 # declaración permite que la función de Lambda cree grupos
 #  de registros, cree flujos de registros y transfiera eventos 
 #  de registro a CloudWatch Logs
-data "aws_iam_policy_document" "ws_lambda_policy" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    effect   = "Allow"
-    resource = ["arn:aws:logs:*:*:*"]
-  }
+# data "aws_iam_policy_document" "ws_lambda_policy" {
+#   statement {
+#     actions = [
+#       "logs:CreateLogGroup",
+#       "logs:CreateLogStream",
+#       "logs:PutLogEvents"
+#     ]
+#     effect   = "Allow"
+#     resource = ["arn:aws:logs:*:*:*"]
+#   }
 
 # permite que la función Lambda coloque elementos,
 #  elimine elementos y escanee el contenido de una tabla de Amazon DynamoDB
@@ -48,61 +48,46 @@ data "aws_iam_policy_document" "ws_lambda_policy" {
   # }
 
 # declaración permite que la función Lambda ejecute API en API Gateway
-  statement {
-    actions = [
-      "execute-api:*",
-    ]
-    effect   = "Allow"
-    resource = [
-      "${aws_apigatewayv2_stage.ws_messenger_api_stage.execution_arn}/PING"
-    ] 
-  }
+#   statement {
+#     actions = [
+#       "execute-api:*",
+#     ]
+#     effect   = "Allow"
+#     resource = [
+#       "${aws_apigatewayv2_stage.ws_messenger_api_stage.execution_arn}/*/*/*"
+#     ] 
+#   }
 
-}
-
-data  "aws_iam_policy_document" "ws_messeger_apigateway_policy" {
-  statement {
-    actions = [
-      "lambda:InvokeFunction",
-    ]
-    effect   = "Allow"
-    resource = [aws_lambda_function.ws_go_chat.arn]
-  }
-}
-
-resource "aws_iam_policy" "ws_messenger_lambda_policy" {
-  name   = "WsMessengerLambdaPolicy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.ws_messenger_lambda_policy.json
-}
-
-resource "aws_iam_policy" "ws_messenger_api_gateway_policy" {
-  name   = "WsMessengerAPIGatewayPolicy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.ws_messenger_api_gateway_policy.json
-}
+# }
 
 
-resource "aws_iam_role" "ws_messenger_api_gateway_role" {
-  name = "WsMessengerAPIGatewayRole"
+# data  "aws_iam_policy_document" "ws_messeger_apigateway_policy" {
+#   statement {
+#     actions = [
+#       "lambda:InvokeFunction",
+#     ]
+#     effect   = "Allow"
+#     resource = [aws_lambda_function.ws_lambda_messeger.arn]
+#   }
+# }
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "apigateway.amazonaws.com"
-        }
-      },
-    ]
-  })
 
-  managed_policy_arns = [aws_iam_policy.ws_messenger_api_gateway_policy.arn]
-}
+# ##############################################################
+# Copilacion del codigo                                        #
+# en esta parte pondremos que se copile el codigo               
+# y se guarde en el zip, mediante terraform, sin necesidad de 
+# que nosotros tengamos que estar haciendolo manualmente, 
+# si no solo con el comando terraform apply se construye toda 
+# la infraestructura del codgio como el del despliegue
+# resource "null_resource" "compile" {
+#     trigger = {
+#         build_number = "${timestamp()}"
+#     }
 
+#     provider "local-exec" {
+#         command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ./main -ldflags '-w' ./main.go"
+#     }
+# }
 
 
 # ###################################################
@@ -111,46 +96,54 @@ resource "aws_iam_role" "ws_messenger_api_gateway_role" {
 #                                                   #
 ####################################################
 
-resource "aws_iam_role" "chat_go" {
-  name               = "chat_go"
+resource "aws_iam_role" "examplego" {
+  name               = "examplego"
   assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Action": "sts:AssumeRole",
-    "Principal": {
-      "Service": "lambda.amazonaws.com"
-    },
-    "Effect": "Allow"
+  {
+    "Version": "2012-10-17",
+    "Statement": {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
   }
-}
-POLICY
+  POLICY
 }
 
 # ##################################################
 #                                                  #
 # Se crea la funcion Lambda para                   #
 ####################################################
+resource "null_resource" "function_binary" {
+  provisioner "local-exec" {
+    command = "GOOS=linux GOARCH=amd64 go build -o ../main ../main.go"
+  }
+}
+
+data "archive_file" "zip" {    
+    depends_on = [null_resource.function_binary]
+    type        = "zip"
+    source_file = "../main"
+    output_path = "../main.zip"
+}
+
+
 resource "aws_lambda_function" "ws_go_chat" {
     function_name     = "go_chat"
     runtime           = "go1.x"
-    filename          = "main.zip"
+    filename          = "../main.zip"
     handler           = "main"
-    role              = aws_iam_role.chat_go.arn
-    source_code_hash  = sha256(filebase64("main.zip"))
+    role              = aws_iam_role.examplego.arn
+    source_code_hash = "data.archive_file.zip.output_base64sha256"
+    #source_code_hash  = sha256(filebase64("../main.zip"))
     memory_size       = 128
     timeout           = 10
-
-
-    environment {
-      variables = {
-        "API_GATEWAY_ENDPOINT" = "https://${aws_apigatewayv2_api.websocket_go.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.ws_messenger_api_stage.id}"
-        # "DYNAMODB_TABLE"       = aws_dynamodb_table.ws_messenger_table.id
-      }
-    }
 }
 
 # Creamos unos logs, con aws_cloudwatch_log_group
+
 # resource "aws_cloudwatch_log_group" "ws_messenger_logs" {
 #   name              = "/aws/lambda/${aws_lambda_function.ws_go_chat.function_name}"
 #   retention_in_days = 7
@@ -175,63 +168,92 @@ resource "aws_apigatewayv2_api" "websocket_go" {
 }
 
 # la aws_apigatewayv2_integration es para estar atento a los servicios que se solicita 
-resource "aws_apigatewayv2_integration" "ws_messenger_api_integration" {
-  api_id                    = aws_apigatewayv2_api.websocket_go.id
-  integration_type          = "AWS_PROXY"
-  integration_uri           = aws_lambda_function.ws_go_chat.invoke_arn
-  # credentials_arn           = aws_iam_role.ws_messenger_api_gateway_role.arn
+resource "aws_apigatewayv2_integration" "lambda_main" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  integration_uri    = aws_lambda_function.ws_go_chat.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
   content_handling_strategy = "CONVERT_TO_TEXT"
   passthrough_behavior      = "WHEN_NO_MATCH"
-}
-
-resource "aws_apigatewayv2_route" "ws_messenger_api_ping_route" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "PING"
-  target    = "integrations/${aws_apigatewayv2_integration.ws_messenger_api_integration.id}"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_messenger_api_ping_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_messenger_api_ping_route.id
-  route_response_key = "$default"
-}
-
-resource "aws_apigatewayv2_stage" "ws_messenger_api_stage" {
-  api_id      = aws_apigatewayv2_api.websocket_go.id
-  name        = "developers"
-  auto_deploy = true
 }
 
 # aqui se reenvia las solicitudes especiales ($connect, $disconnect)
 # para nuestra funcion lambda para poder administrar los estados de la lambda
 # serian las subrutas que tiene la apigatewayv2
-# resource "aws_apigatewayv2_route" "ws_connect" {
-#   api_id    = aws_apigatewayv2_api.websocket_go.id
-#   route_key = "$connect"
-#   target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-# }
+resource "aws_apigatewayv2_route" "ws_connect" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$connect"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
 
-# resource "aws_apigatewayv2_route" "ws_disconnect" {
-#   api_id    = aws_apigatewayv2_api.websocket_go.id
-#   route_key = "$disconnect"
-#   target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-# }
+resource "aws_apigatewayv2_route" "ws_disconnect" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$disconnect"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
 
-# resource "aws_apigatewayv2_route" "ws_default" {
-#   api_id    = aws_apigatewayv2_api.websocket_go.id
-#   route_key = "$default"
-#   target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-# }
+resource "aws_apigatewayv2_route" "ws_default" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
 
+resource "aws_apigatewayv2_route" "ws_ping_route" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "PING"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+################################
+resource "aws_apigatewayv2_route" "ws_message_route" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "MESSAGE"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
 
-# ahora vamos hacer una implementacion "real" 
-# resource "aws_apigatewayv2_stage" "lambda" {
-#   api_id      = aws_apigatewayv2_api.websocket_go.id
-#   name        = "ws_primary"
-#   auto_deploy = true
-# }
+resource "aws_apigatewayv2_integration_response" "ws_messenger_api_integration_response" {
+  api_id                   = aws_apigatewayv2_api.websocket_go.id
+  integration_id           = aws_apigatewayv2_integration.lambda_main.id
+  integration_response_key = "/200/"
+}
 
-# Allow the API Gateway to invoke Lambda function
+resource "aws_apigatewayv2_route_response" "ws_connect_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_connect.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_disconnect_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_disconnect.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_ping_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_ping_route.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_message_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_message_route.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_default_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_default.id
+  route_response_key = "$default"
+}
+##############################
+# # ahora vamos hacer una implementacion "real" 
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id      = aws_apigatewayv2_api.websocket_go.id
+  name        = "ws_primary"
+  auto_deploy = true
+}
+
+# # Allow the API Gateway to invoke Lambda function
 resource "aws_lambda_permission" "ws_messenger_permission" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
