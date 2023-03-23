775c41dd9de6254b88e10d6277abe03bb0853b8a
Ericka <erickaramos@facephi.com>
2023-03-23 14:39:33 +0100

    added infrastructure

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,25 +1,106 @@
-# terraform {
-#     required_providers {
-#         aws = {
-#             source  = "hashicorp/aws"
-#             version = "~> 4.16"
-#         }
-#     }
-
-#     backend "s3" {
-#       bucket = "local-state"
-#       key    = "messeger/terraform.tfstate"
-#       region = var.aws_region
-#     }
-    
-    
-
-#   required_version = ">= 0.12"
-# }
+# ##################################################
+#                                                  #
+# Aqui creamos un APIGATEWAY para la lambda  
+# loi que importa aqui en la palabra WEBSOCKET,
+# dentro de aqui hay varios enrutamientos
+# En esta expresion "$request.body.action", deciamos
+# que en el bodyde la peticion hay una action,
+# ahi es donde genera el enrutamiento
+####################################################
 
 
-# provider "aws" {
-#     region = var.aws_region
-# }
+
+# la aws_apigatewayv2_integration es para estar atento a los servicios que se solicita 
+resource "aws_apigatewayv2_integration" "lambda_main" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  integration_uri    = aws_lambda_function.ws_go_chat.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "POST"
+  content_handling_strategy = "CONVERT_TO_TEXT"
+  passthrough_behavior      = "WHEN_NO_MATCH"
+}
 
 
+##############################
+# # ahora vamos hacer una implementacion "real" 
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id      = aws_apigatewayv2_api.websocket_go.id
+  name        = "ws_primary"
+  auto_deploy = true
+}
+
+resource "aws_apigatewayv2_api" "websocket_go" {
+    name                       = "ws_messenger_api"
+    description                = "Send data for the WS"
+    protocol_type              = "WEBSOCKET"
+    route_selection_expression = "$request.body.action"
+}
+
+# aqui se reenvia las solicitudes especiales ($connect, $disconnect)
+# para nuestra funcion lambda para poder administrar los estados de la lambda
+# serian las subrutas que tiene la apigatewayv2
+resource "aws_apigatewayv2_route" "ws_connect" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$connect"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+
+resource "aws_apigatewayv2_route" "ws_disconnect" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$disconnect"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+
+resource "aws_apigatewayv2_route" "ws_default" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+
+resource "aws_apigatewayv2_route" "ws_ping_route" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "PING"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+################################
+resource "aws_apigatewayv2_route" "ws_message_route" {
+  api_id    = aws_apigatewayv2_api.websocket_go.id
+  route_key = "MESSAGE"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
+}
+
+resource "aws_apigatewayv2_integration_response" "ws_messenger_api_integration_response" {
+  api_id                   = aws_apigatewayv2_api.websocket_go.id
+  integration_id           = aws_apigatewayv2_integration.lambda_main.id
+  integration_response_key = "/200/"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_connect_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_connect.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_disconnect_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_disconnect.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_ping_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_ping_route.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_message_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_message_route.id
+  route_response_key = "$default"
+}
+
+resource "aws_apigatewayv2_route_response" "ws_default_route_response" {
+  api_id             = aws_apigatewayv2_api.websocket_go.id
+  route_id           = aws_apigatewayv2_route.ws_default.id
+  route_response_key = "$default"
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -24,28 +24,28 @@ provider aws {
 # declaración permite que la función de Lambda cree grupos
 #  de registros, cree flujos de registros y transfiera eventos 
 #  de registro a CloudWatch Logs
-# data "aws_iam_policy_document" "ws_lambda_policy" {
-#   statement {
-#     actions = [
-#       "logs:CreateLogGroup",
-#       "logs:CreateLogStream",
-#       "logs:PutLogEvents"
-#     ]
-#     effect   = "Allow"
-#     resource = ["arn:aws:logs:*:*:*"]
-#   }
+data "aws_iam_policy_document" "ws_lambda_policy_document" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect   = "Allow"
+    resources = ["arn:aws:logs:*:*:*"]
+  }
 
 # permite que la función Lambda coloque elementos,
 #  elimine elementos y escanee el contenido de una tabla de Amazon DynamoDB
-  # statement {
-  #   actions = [
-  #     "dynamodb:PutItem",
-  #     "dynamodb:DeleteItem",
-  #     "dynamodb:Scan",
-  #   ]
-  #   effect   = "Allow"
-  #   resource = [aws_dynamodb_table.ws_messenger_table.arn]
-  # }
+  statement {
+    actions = [
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem",
+      "dynamodb:Scan",
+    ]
+    effect   = "Allow"
+    resources = [aws_dynamodb_table.ws_table.arn]
+  }
 
 # declaración permite que la función Lambda ejecute API en API Gateway
 #   statement {
@@ -58,7 +58,13 @@ provider aws {
 #     ] 
 #   }
 
-# }
+}
+
+resource "aws_iam_policy" "ws_lambda_policy" {
+  name   = "WsMessengerLambdaPolicy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.ws_lambda_policy_document.json
+}
 
 
 # data  "aws_iam_policy_document" "ws_messeger_apigateway_policy" {
@@ -110,6 +116,7 @@ resource "aws_iam_role" "examplego" {
     }
   }
   POLICY
+  managed_policy_arns = [aws_iam_policy.ws_lambda_policy.arn]
 }
 
 # ##################################################
@@ -138,119 +145,21 @@ resource "aws_lambda_function" "ws_go_chat" {
     role              = aws_iam_role.examplego.arn
     source_code_hash = "data.archive_file.zip.output_base64sha256"
     #source_code_hash  = sha256(filebase64("../main.zip"))
+    environment {
+      variables = {
+        "API_GATEWAY_ENDPOINT" = "https://${aws_apigatewayv2_api.websocket_go.id}.execute-api.eu-west-2.amazonaws.com/${aws_apigatewayv2_stage.lambda.id}"
+        "DYNAMODB_TABLE"       = aws_dynamodb_table.ws_table.id
+      }
+    }
     memory_size       = 128
     timeout           = 10
 }
 
 # Creamos unos logs, con aws_cloudwatch_log_group
 
-# resource "aws_cloudwatch_log_group" "ws_messenger_logs" {
-#   name              = "/aws/lambda/${aws_lambda_function.ws_go_chat.function_name}"
-#   retention_in_days = 7
-# }
-
-
-# ##################################################
-#                                                  #
-# Aqui creamos un APIGATEWAY para la lambda  
-# loi que importa aqui en la palabra WEBSOCKET,
-# dentro de aqui hay varios enrutamientos
-# En esta expresion "$request.body.action", deciamos
-# que en el bodyde la peticion hay una action,
-# ahi es donde genera el enrutamiento
-####################################################
-
-resource "aws_apigatewayv2_api" "websocket_go" {
-    name                       = "ws_messenger_api"
-    description                = "Send data for the WS"
-    protocol_type              = "WEBSOCKET"
-    route_selection_expression = "$request.body.action"
-}
-
-# la aws_apigatewayv2_integration es para estar atento a los servicios que se solicita 
-resource "aws_apigatewayv2_integration" "lambda_main" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  integration_uri    = aws_lambda_function.ws_go_chat.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-  content_handling_strategy = "CONVERT_TO_TEXT"
-  passthrough_behavior      = "WHEN_NO_MATCH"
-}
-
-# aqui se reenvia las solicitudes especiales ($connect, $disconnect)
-# para nuestra funcion lambda para poder administrar los estados de la lambda
-# serian las subrutas que tiene la apigatewayv2
-resource "aws_apigatewayv2_route" "ws_connect" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "$connect"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-}
-
-resource "aws_apigatewayv2_route" "ws_disconnect" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "$disconnect"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-}
-
-resource "aws_apigatewayv2_route" "ws_default" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "$default"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-}
-
-resource "aws_apigatewayv2_route" "ws_ping_route" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "PING"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-}
-################################
-resource "aws_apigatewayv2_route" "ws_message_route" {
-  api_id    = aws_apigatewayv2_api.websocket_go.id
-  route_key = "MESSAGE"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_main.id}"
-}
-
-resource "aws_apigatewayv2_integration_response" "ws_messenger_api_integration_response" {
-  api_id                   = aws_apigatewayv2_api.websocket_go.id
-  integration_id           = aws_apigatewayv2_integration.lambda_main.id
-  integration_response_key = "/200/"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_connect_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_connect.id
-  route_response_key = "$default"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_disconnect_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_disconnect.id
-  route_response_key = "$default"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_ping_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_ping_route.id
-  route_response_key = "$default"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_message_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_message_route.id
-  route_response_key = "$default"
-}
-
-resource "aws_apigatewayv2_route_response" "ws_default_route_response" {
-  api_id             = aws_apigatewayv2_api.websocket_go.id
-  route_id           = aws_apigatewayv2_route.ws_default.id
-  route_response_key = "$default"
-}
-##############################
-# # ahora vamos hacer una implementacion "real" 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id      = aws_apigatewayv2_api.websocket_go.id
-  name        = "ws_primary"
-  auto_deploy = true
+resource "aws_cloudwatch_log_group" "ws_messenger_logs" {
+  name              = "/aws/lambda/${aws_lambda_function.ws_go_chat.function_name}"
+  retention_in_days = 7
 }
 
 # # Allow the API Gateway to invoke Lambda function
@@ -262,4 +171,20 @@ resource "aws_lambda_permission" "ws_messenger_permission" {
   source_arn    = "${aws_apigatewayv2_api.websocket_go.execution_arn}/*/*"
 }
 
+resource "aws_dynamodb_table" "ws_table" {
+  name           = "websocket-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 2
+  write_capacity = 2
+  hash_key       = "ConnectionID"
 
+  attribute {
+    name = "ConnectionID"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "ExpirationTime"
+    enabled        = true
+  }
+}
